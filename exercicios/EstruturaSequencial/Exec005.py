301e80636360a8d6cb7d9a722654967e228bf61d
geosilva <vanegmsf@gmail.com>
2018-12-18 12:44:58 -0200

    at

--- exercicios/EstruturaSequencial/Exec005.py
+++ exercicios/EstruturaSequencial/Exec005.py
@@ -1,3 +1,7 @@
 #-*- coding: utf-8 -*-
 
 #Faça um Programa que converta metros para centímetros.
+
+metros = float(input("Metros: "))
+centimetros = metros*100
+print("{} é igual à {} cm".format(metros, centimetros))
